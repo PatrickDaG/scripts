3331ae4769bf531a090fcfb4b759e03c090d644b
Patrick Grossmann <patrickdgro@gmail.com>
2022-01-08 14:13:10 +0100

    feat: battery_warn no longer prints error if no battery found

--- battery_warn.sh
+++ battery_warn.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # small script to send notification on battery almost empty or full
+if ! test -d /sys/class/power_supply/BAT0; then
+	echo ""
+	return
+fi
 current=$(cat /sys/class/power_supply/BAT0/capacity)
-if [ "$current" -lt 10 ]
+status=$(cat /sys/class/power_supply/BAT0/status)
+if [[ "$current" -lt 10 ]] && [[ "$status" == "Discharging" ]]
 then
-	notify-send "Die Battery ist eher leer. Sollte mal gelade werden"
-elif [ "$current" -gt 90 ]
+	notify-send "Die Battery ist eher leer. Sollte mal gelade werden" \
+		-u critical
+elif [[ "$current" -gt 90 ]] && [[ "$status" == "Charging" ]]
 then
-	notify-send "Reicht auch schon wieder mit dem laden"
+	notify-send "Reicht auch schon wieder mit dem laden" \
+		-u normal
 fi
