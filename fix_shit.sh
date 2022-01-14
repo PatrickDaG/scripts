54b621f830751e03138b75f6b632fb9795183481
Patrick Grossmann <patrickdgro@gmail.com>
2022-01-14 21:36:38 +0100

    feat: added fix for stylus

--- fix_shit.sh
+++ fix_shit.sh
@@ -5,4 +5,5 @@ herbstclient detect_monitors
 ${HOME}.config/polybar/launch.sh &>/dev/null
 xinput --map-to-output "ELAN2514:00 04F3:2817" eDP-1
 xinput --map-to-output "ELAN2514:00 04F3:2817 Stylus Pen (0)" eDP-1
+xinput --map-to-output "ELAN2514:00 04F3:2817 Stylus Eraser (0)" eDP-1
 xset r rate 235 60
